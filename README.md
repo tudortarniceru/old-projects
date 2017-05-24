69f66a7808a293977d8ccd2327af23843e95c10a
Tudor Tarniceru <tudortarniceru@yahoo.com>
2017-05-24 21:58:25 +0300

    My Website

--- README.md
+++
@@ -1,6 +0,0 @@
-## License
-
-Copyright (c) 2016 Tudor Tarniceru
-
-This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
-
---
+++ colorscheme.js
@@ -0,0 +1,28 @@
+var color_scheme_nr = 2;
+var color_scheme_name = ['', 'Dark', 'Light'];
+
+if (localStorage.getItem("color_scheme") == null) {
+    localStorage.setItem("color_scheme", 1);
+}
+
+function change_color_scheme(nr) {
+    localStorage["color_scheme"] = nr;
+    document.getElementById("css").href = "style" + nr + ".css";
+    show_color_schemes();
+}
+
+function show_color_schemes() {
+    var txt = '';
+    var current_color_scheme = localStorage.getItem("color_scheme");
+    for (i = 1; i <= color_scheme_nr; ++i) {
+        if (i == current_color_scheme) {
+            txt += '<a class="dropdown-active" onclick="change_color_scheme(' + i + ')">' + color_scheme_name[i] + '</a>';
+        }
+        else {
+            txt += '<a onclick="change_color_scheme(' + i + ')">' + color_scheme_name[i] + '</a>';
+        }
+    }
+    document.getElementById("color_scheme_list").innerHTML = txt;
+}
+
+change_color_scheme(localStorage.getItem("color_scheme"));
